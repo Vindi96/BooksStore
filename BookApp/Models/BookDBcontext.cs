c38b6f19e3533bf797d84c3340d59af206aabef7
Vindi96 <vindya.madhushani96@gmail.com>
2021-01-20 00:40:26 +0530

    create migration class

---
+++ BookApp/Models/BookDBcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Models
+{
+    public class BookDBcontext:DbContext
+    {
+        public DbSet <Publisher>Publishers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\sqlexpress;Initial Catalog=BooksDB;Integrated Security=True");
+        }
+    }
+}
